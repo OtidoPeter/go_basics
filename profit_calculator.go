e9364145a255ef5470be0d956c2e75623f2d6f2b
Otido Peter <peterotido@gmail.com>
2025-05-20 07:19:52 +0300

    more functions

--- profit_calculator.go
+++ profit_calculator.go
@@ -5,9 +5,9 @@ import (
 )
 
 func main() {
+	var taxRate float64
 	var revenue float64
 	var expenses float64
-	var taxRate float64
 
 	// fmt.Print("Revenue: ")
 	outputNum("Revenue: ")
@@ -21,15 +21,27 @@ func main() {
 	outputNum("Tax rate: ")
 	fmt.Scan(&taxRate)
 
-	earningsBeforeTax := revenue - expenses
-	earningsAfterTax := (earningsBeforeTax) * (1 - taxRate/100)
-	ratio := earningsBeforeTax / earningsAfterTax
-	fmt.Println(earningsBeforeTax)
-	fmt.Println(earningsAfterTax)
-	fmt.Println(ratio)
+	earningsBeforeTax, earningsAfterTax, ratio := calculateEarnings(revenue, expenses, taxRate)
+	// earningsAfterTax := (earningsBeforeTax) * (1 - taxRate/100)
+	// ratio := earningsBeforeTax / earningsAfterTax
+
+	fmt.Printf("Earnings Before Tax: %.2f\n", earningsBeforeTax)
+	fmt.Printf("Earnings After Tax: %.2f\n", earningsAfterTax)
+	fmt.Printf("EBT/After-Tax Ratio: %.2f\n", ratio)
+	//fmt.Println(earningsBeforeTax)
+	//fmt.Println(earningsAfterTax)
+	// fmt.Println(ratio)
 }
 
 func outputNum(num string) {
 	fmt.Print(num)
 
+}
+
+func calculateEarnings(revenue, expenses, taxRate float64) (ebt float64, eat float64, rat float64) {
+	//
+	ebt = revenue - expenses
+	eat = (ebt) * (1 - taxRate/100)
+	rat = ebt / eat
+	return ebt, eat, rat
 }
