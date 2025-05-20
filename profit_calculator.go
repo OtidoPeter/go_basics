62841d07e78bc435a4c8c87a082b83946cac0f27
Otido Peter <peterotido@gmail.com>
2025-05-20 14:01:10 +0300

    more functions

--- profit_calculator.go
+++ profit_calculator.go
@@ -25,9 +25,9 @@ func main() {
 	// earningsAfterTax := (earningsBeforeTax) * (1 - taxRate/100)
 	// ratio := earningsBeforeTax / earningsAfterTax
 
-	fmt.Printf("Earnings Before Tax: %.2f\n", earningsBeforeTax)
-	fmt.Printf("Earnings After Tax: %.2f\n", earningsAfterTax)
-	fmt.Printf("EBT/After-Tax Ratio: %.2f\n", ratio)
+	fmt.Printf("%.1f\n", earningsBeforeTax)
+	fmt.Printf("%.1f\n", earningsAfterTax)
+	fmt.Printf("%.1f\n", ratio)
 	//fmt.Println(earningsBeforeTax)
 	//fmt.Println(earningsAfterTax)
 	// fmt.Println(ratio)
@@ -40,10 +40,10 @@ func outputNum(num string) float64 {
 	return userInput
 }
 
-func calculateEarnings(revenue, expenses, taxRate float64) (ebt float64, eat float64, rat float64) {
+func calculateEarnings(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	//
-	ebt = revenue - expenses
-	eat = (ebt) * (1 - taxRate/100)
-	rat = ebt / eat
+	ebt := revenue - expenses
+	eat := (ebt) * (1 - taxRate/100)
+	rat := ebt / eat
 	return ebt, eat, rat
 }
